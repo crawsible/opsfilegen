b010ca329a74c114980cfae8fcfbed960dfd2538
Colin Jackson <colin.s.jackson90@gmail.com>
2017-07-09 21:40:52 -0400

    Add integration test (currently failing)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Opsfilegen
+## opsfilegen
 Generates an opsfile from the diff between two bosh manifests.
 
 ### Intentional Limitations
@@ -6,6 +6,8 @@ Generates an opsfile from the diff between two bosh manifests.
 
 ### Temporary Limitations
 * *Extremely* hackday code. No tests; one big main.go.
-* Only creates remove definitions.
-* Will only remove array elements if they are hashes; will not create remove definitions for strings or integers, which need to be referenced positionally.
-* Does not prioritize the "name" key when looking for a unique identifier for an array item, so paths sometimes have patterns like "/instances/vm_type=c3.large/stemcell"
+* Only creates remove definitions -- should also create replace definitions
+* Will only remove array elements if they are hashes; will not create remove definitions for strings or integers, which need to be referenced positionally -- should either replace whole array with new array or remove the element by index (seems dangerous)
+* Does not prioritize the "name" key when looking for a unique identifier for an array item, so paths sometimes have patterns like "/instances/vm_type=c3.large/stemcell" -- should look for "name=" unique key first
+* Fails silently and provides an invalid opsfile if YAML variable definition is removed without references being removed -- should fail with descriptive error
+* Definition order is random -- should be top-down, deep first
---
+++ integration/integration_test.go
@@ -0,0 +1,57 @@
+package integration
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/cppforlife/go-patch/patch"
+	"gopkg.in/yaml.v2"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Integration", func() {
+	var opsfilegenPath string
+	BeforeSuite(func() {
+		var err error
+		opsfilegenPath, err = gexec.Build("github.com/crawsible/opsfilegen")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		gexec.CleanupBuildArtifacts()
+	})
+
+	It("generates an opsfile from a source target manifest", func() {
+		wd, _ := os.Getwd()
+
+		sourceManifestPath := filepath.Join(wd, "fixtures/source.yml")
+		targetManifestPath := filepath.Join(wd, "fixtures/target.yml")
+		expectedOpsFilePath := filepath.Join(wd, "fixtures/expected_opsfile.yml")
+		expectedOutput, _ := ioutil.ReadFile(expectedOpsFilePath)
+
+		command := exec.Command(
+			opsfilegenPath,
+			sourceManifestPath,
+			targetManifestPath,
+		)
+
+		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		actualOutput := session.Wait(5 * time.Second).Out.Contents()
+
+		var expectedOpDefs []patch.OpDefinition
+		var actualOpDefs []patch.OpDefinition
+
+		_ = yaml.Unmarshal(expectedOutput, &expectedOpDefs)
+		err = yaml.Unmarshal(actualOutput, &actualOpDefs)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(actualOpDefs).To(ConsistOf(expectedOpDefs))
+	})
+})
